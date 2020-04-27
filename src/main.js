a655303046b6da186538ae45c8cccd43f2330ade
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-04-27 08:19:44 -0700

    initial

--- src/main.js
+++ src/main.js
@@ -1,40 +1,7 @@
-import { pingPong } from './ping-pong';
 import $ from 'jquery';
 import './styles.css';
-import stockPuppyPhoto from './assets/images/puppy.jpg';
 
 $(document).ready(function() {
-  $('#ping-pong-form').submit(function(event) {
-    event.preventDefault();
-    var goal = $('#goal').val();
-    var output = pingPong(goal);
-    output.forEach(function(element) {
-      $('#solution').append("<li>" + element + "</li>");
-    });
-  });
-  
-  $("#thing").html(`<img src=${stockPuppyPhoto} />`);
-
-  $('#weatherLocation').click(function() {
-    const city = $('#location').val();
-    $('#location').val("");
-
-    let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
-
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
-      }
-    }
-
-    request.open("GET", url, true);
-    request.send();
-
-   const getElements = function(response) {
-      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
-    }
-  });
+  $('#').submit(function(event) {
+  }
 });
