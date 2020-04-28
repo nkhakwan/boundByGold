db03a7e7911906c319659d0addc288c088832ea0
nkhakwan <post_khan@yahoo.com>
2020-04-27 18:45:38 -0700

    committing code changes done to src files

--- src/contracts.js
+++ src/contracts.js
@@ -1,101 +1,91 @@
 export class JobBoard {
-  constructor (start) {
-    this.start = start;
-  };
-
-  werewolf() {
-    let numberOfWolvesContracts = randomGen(3);
-    let numberOfWolves = [0][0];
-    for (let i=1; i<numberOfWolvesContracts.length+1; i=i+1){
-      numberOfWolves[0][i] = randomGen(5);
-      numberOfWolves[1][i] = ((randomGen(5)*numberOfWolves[0][i])+300);
-    }
-    return numberOfWolves;
-  };
   
-  goblinCave() {
-    let numberOfGoblinContracts = randomGen(2);
-    let numberOfGoblins = [0][0];
-    for (let i=1; i<numberOfGoblinContracts.length+1; i=i+1){
-      numberOfGoblins[0][i] = randomGen(12);
-      numberOfGoblins[1][i] = numberOfGoblins[0][i] * 25;
-    }
-    return numberOfGoblins;
-  };
-  
-  caravanEscort() {
-    let numberOfEscortContracts = randomGen(7);
-    let numberOfBandits = [0][0];
-    for (let i=1; i<numberOfEscortContracts.length+1; i=i+1){
-      numberOfBandits[0][i] = randomGen(10);
-      numberOfBandits[1][i] = numberOfBandits[0][i] * 50;
-    }
-    return numberOfBandits;
-  };
 
-  caravanAmbush() {
-    let numberOfAmbushContracts = randomGen(2);
-    let numberOfGuards = [0][0];
-    for (let i=1; i<numberOfAmbushContracts.length+1; i=i+1){
-      numberOfGuards[0][i] = randomGen(9);
-      numberOfGuards[1][i] = (numberOfGuards[0][i]*this.randomGen(5))+500;
-    }
-    return numberOfGuards;
-  };
 
-  manhunterContract() {
-    let numberOfManhunterContracts = randomGen(4);
-    let numberOfManhunters = [0][0];
-    for (let i=1; i<numberOfManhunterContracts.length+1; i=i+1){
-      numberOfManhunters[0][i] = randomGen(10);
-      numberOfManhunters[1][i] = (numberOfManhunters[0][i]*this.randomGen(5))+750;
-    }
-    return numberOfManhunters;
-  };
 
-  soldierContract() {
-    let numberOfSoldierContracts = randomGen(3);
-    let numberOfSoldiers = [0][0];
-    for (let i=1; i<numberOfSoldierContracts.length+1; i=i+1){
-      numberOfSoldiers[0][i] = randomGen(12);
-      numberOfSoldiers[1][i] = (numberOfSoldiers[0][i]*this.randomGen(7))+1000;
-    }
-    return numberOfSoldiers;
-  };
+  constructor(){
+  //callOurFunctions(){
 
-  ogreContract() {
-    let numberOfOgreContracts = randomGen(2);
-    let numberOfOgres = [0][0];
-    for (let i=1; i<numberOfOgreContracts.length+1; i=i+1){
-      numberOfOgres[0][i] = randomGen(8);
-      numberOfOgres[1][i] = (numberOfOgres[0][i]*this.randomGen(3))+1500;
-    }
-    return numberOfOgres;
-  };
-
-  warPartyContract() {
-    let numberOfWarPartyContracts = randomGen(1);
-    let numberOfWarParty = [0][0];
-    for (let i=1; i<numberOfWarPartyContracts.length+1; i=i+1){
-      numberOfWarParty[0][i] = randomGen(15);
-      numberOfWarParty[1][i] = (numberOfWarParty[0][i]*this.randomGen(15))+2500;
-    }
-    return numberOfWarParty;
-  };
-
-  deathMoney() {
+    this.ourThreeDimentionalArray = new Array(new Array(new Array()));
     this.werewolfDeathMoney = 150;
     this.goblinCaveDeathMoney = 75;
     this.caravanEscortDeathMoney = 125;
     this.caravanAmbushDeathMoney = 200;
-    this.manhunterContractDeathMoney = 350;
+    this.manHunterContractDeathMoney = 350;
     this.guardPatrolContractDeathMoney = 425;
     this.ogreContractDeathMoney = 500;
     this.mercContractDeathMoney = 750;
-  };
+  }
+
+  werewolf() {
+    let numberOfWolvesContracts = this.randomGen(3);
+    for (let i=0; i<numberOfWolvesContracts; i=i+1){
+      this.ourThreeDimentionalArray[0][0][i] = this.randomGen(5);
+      console.log(`we are here ${this.ourThreeDimentionalArray[0][0][i]}`);
+      //this.ourThreeDimentionalArray[0][1][i] = ((this.randomGen(5)*(this.ourThreeDimentionalArray[0][0][i]))+300);
+      let a = ((this.randomGen(5)*(this.ourThreeDimentionalArray[0][0][i]))+300);
+      console.log(a);
+    }
+  }
+  
+  goblinCave() {
+    let numberOfGoblinContracts = this.randomGen(2);
+    for (let i=0; i<numberOfGoblinContracts.length; i=i+1){
+      this.ourThreeDimentionalArray[1][0][i] = this.randomGen(12);
+      this.ourThreeDimentionalArray[1][1][i] = this.ourThreeDimentionalArray[1][0][i] * 25;
+    }
+  }
+  
+  caravanEscort() {
+    let numberOfEscortContracts = this.randomGen(7);
+    for (let i=0; i<numberOfEscortContracts.length; i=i+1){
+      this.ourThreeDimentionalArray[2][0][i] = this.randomGen(10);
+      this.ourThreeDimentionalArray[2][1][i] = this.ourThreeDimentionalArray[2][0][i] * 50;
+    }
+  }
+
+  caravanAmbush() {
+    let numberOfAmbushContracts = this.randomGen(2);
+    for (let i=0; i<numberOfAmbushContracts.length; i=i+1){
+      this.ourThreeDimentionalArray[3][0][i] = this.randomGen(9);
+      this.ourThreeDimentionalArray[3][1][i] = (this.ourThreeDimentionalArray[3][0][i]*this.randomGen(5))+500;
+    }
+  }
+
+  manHunterContract() {
+    let numberOfManhunterContracts = this.randomGen(4);
+    for (let i=0; i<numberOfManhunterContracts.length; i=i+1){
+      this.ourThreeDimentionalArray[4][0][i] = this.randomGen(10);
+      this.ourThreeDimentionalArray[4][1][i] = (this.ourThreeDimentionalArray[4][0][i]*this.randomGen(5))+750;
+    }
+  }
+
+  soldierContract() {
+    let numberOfSoldierContracts = this.randomGen(3);
+    for (let i=0; i<numberOfSoldierContracts.length; i=i+1){
+      this.ourThreeDimentionalArray[5][0][i] = this.randomGen(12);
+      this.ourThreeDimentionalArray[5][1][i] = (this.ourThreeDimentionalArray[5][0][i]*this.randomGen(7))+1000;
+    }
+  }
+
+  ogreContract() {
+    let numberOfOgreContracts = this.randomGen(2);
+    for (let i=0; i<numberOfOgreContracts.length; i=i+1){
+      this.ourThreeDimentionalArray[6][0][i] = this.randomGen(8);
+      this.ourThreeDimentionalArray[6][1][i] = (this.ourThreeDimentionalArray[6][0][i]*this.randomGen(3))+1500;
+    }
+  }
+
+  warPartyContract() {
+    let numberOfWarPartyContracts = this.randomGen(1);
+    for (let i=0; i<numberOfWarPartyContracts.length; i=i+1){
+      this.ourThreeDimentionalArray[7][0][i] = this.randomGen(15);
+      this.ourThreeDimentionalArray[7][1][i] = (this.ourThreeDimentionalArray[7][0][i]*this.randomGen(15))+2500;
+    }
+  }
 
   randomGen(number) {
     return Math.ceil(Math.random() * number);
-  };
+  }
   
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
 import $ from 'jquery';
 import './styles.css';
+import { JobBoard } from './contracts.js';
 
 $(document).ready(function() {
-  $('#').submit(function(event) {
+  $('#gameStart').click(function() {
+    console.log(`we are in click function`);
+    let jobBoard = new JobBoard;
+    jobBoard.werewolf();
+    console.log(`see this ${jobBoard.ourThreeDimentionalArray[0][0][0]}`);
+    for (let i=0; i<8; i=i+1) {
+      console.log(`we are in first loop`);
+      //for (let j=0; j<jobBoard.callOurFunctions.ourThreeDimentionalArray[i][0].length; j=j+1){
+        for (let j=0; j<1; j=j+1){
+        console.log(`we are in the second loop`);
+        for (let k=0; k<2; k=k+1){
+          console.log(`we are in the final loop`);
+          console.log(`here is the hard data ${jobBoard.ourThreeDimentionalArray[i][k][j]}`);
+        $("#showOutput").html(jobBoard.ourThreeDimentionalArray[i][k][j]);
+      }
+    }
   }
+});
 });
