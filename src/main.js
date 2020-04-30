13c9fc0f5512ea4b6d2621a3272aaff66d75fbfc
nkhakwan <post_khan@yahoo.com>
2020-04-29 17:24:57 -0700

    committing reduced code by Travis

--- src/main.js
+++ src/main.js
@@ -18,6 +18,7 @@ function displayArmy(army, mercTier) {
 $(document).ready(function () {
   let jobBoard = new JobBoard;
   let incomeStatement = new IncomeStatement;
+
   let combat = new Combat;
   let names = ["Hernias the Sad", "Gilbert Greymane", "Willmot Hightower", "Isemberd the Just", "Hameline of the Golden Plains", "Bryant Tykum", "Maximus Decimus Meridius", "Varog un Thuul", "Jareth Ironskin", "Hagalbar af Baggleton", "Tyvik af Montainius", "Renham the Fat", "Jerrick the Prick", "Quintis Veryartus", "Huidemar Greenshield", "Richemanus of Butterhall", "Neale the Strong", "Acur the Dimwitted", "Igor Malculinus", "Tamas the Tiny", "Federyc the Ulfhednar", "Timaeus Garoland", "Hephaeus the Smith", "Phaedrus the Shadow", "Foust the Fairhaired", "Ulrick Von Likteinstein", "Leonitus the Pure", "Marcus Orelious Krasis", "Roger Osprey", "Sir Gwendalin Gant", "Titus Yongrel", "Philip the Tall", "Dunk the Drunk", "Hilder the Brute", "Ulf the Starry Eyed", "Krixis the Bull", "Gannocus the Undeafeated", "Rikard the Swift", "Ike of Vundeburg"];
   let coinBag = 0;
@@ -58,64 +59,72 @@ $(document).ready(function () {
     combat.combatLog = [];
     jobBoard.callOurFunctions();
     incomeStatement.aggregateIncome = 0;
+    console.log(`here is the aggregate income on game start in main function ${incomeStatement.aggregateIncome}`);
+    $("#showIncomeEarnedOnContract").html(incomeStatement.aggregateIncome);
 
+    const contractArray = ["WereWolf", "Goblin", "Caravan Escort", "Caravan Ambush", "ManHunter", "Soldier", "Ogre", "WarParty"];
     for (let i = 0; i < 8; i = i + 1) {
-      if (i === 0) {
-        $("#showOutput0").html("");
-        $("#showOutput0").append("<h3>WereWolf contracts are here</h3>");
-        for (let l = 0; l < jobBoard.ourThreeDimentionalArray[i][0].length; l = l + 1) {
-          $("#showOutput0").append(`<li>Enemies present are ${jobBoard.ourThreeDimentionalArray[i][0][l]} werewolves and the contract is worth ${jobBoard.ourThreeDimentionalArray[i][1][l]}g, with death money valued at ${jobBoard.werewolfDeathMoney} gold. <button class="scb werewolf" id="${i},${l}">Select Contract</button></li>`);
-        }
+      $("#showOutput" + i).html("");
+      $("#showOutput0").append("<h3>" + contractArray[i] + " contracts are here</h3>");
+      for (let l = 0; l < jobBoard.ourThreeDimentionalArray[i][0].length; l = l + 1) {
+        $("#showOutput0").append(`<li>Enemies present are ${jobBoard.ourThreeDimentionalArray[i][0][l]} werewolves and the contract is worth ${jobBoard.ourThreeDimentionalArray[i][1][l]}g, with death money valued at ${jobBoard.werewolfDeathMoney} gold. <button class="scb werewolf" id="${i},${l}">Select Contract</button></li>`);
       }
-      if (i === 1) {
-        $("#showOutput1").html("");
-        $("#showOutput1").append("<h3>Goblin contracts are here</h3>");
-        for (let l = 0; l < jobBoard.ourThreeDimentionalArray[i][0].length; l = l + 1) {
-          $("#showOutput1").append(`<li> Enemies present are ${jobBoard.ourThreeDimentionalArray[i][0][l]} goblins and the contract is worth ${jobBoard.ourThreeDimentionalArray[i][1][l]}g, with death money valued at ${jobBoard.goblinCaveDeathMoney} gold. <button class="scb goblin" id="${i},${l}">Select Contract</button></li>`);
-        }
-      }
-      if (i === 2) {
-        $("#showOutput2").html("");
-        $("#showOutput2").append("<h3>Caravan Escort contracts are here</h3>");
-        for (let l = 0; l < jobBoard.ourThreeDimentionalArray[i][0].length; l = l + 1) {
-          $("#showOutput2").append(`<li> Enemies present are ${jobBoard.ourThreeDimentionalArray[i][0][l]} bandits and the contract is worth ${jobBoard.ourThreeDimentionalArray[i][1][l]}g, with death money valued at ${jobBoard.caravanEscortDeathMoney} gold. <button class="scb caravanEscort" id="${i},${l}">Select Contract</button></li>`);
-        }
-      }
-      if (i === 3) {
-        $("#showOutput3").html("");
-        $("#showOutput3").append("<h3>Caravan Ambush contracts are here</h3>");
-        for (let l = 0; l < jobBoard.ourThreeDimentionalArray[i][0].length; l = l + 1) {
-          $("#showOutput3").append(`<li> Enemies present are ${jobBoard.ourThreeDimentionalArray[i][0][l]} caravan guards and the contract is worth ${jobBoard.ourThreeDimentionalArray[i][1][l]}g, with death money valued at ${jobBoard.caravanAmbushDeathMoney} gold. <button class="scb caravanAmbush" id="${i},${l}">Select Contract</button></li>`);
-        }
-      }
-      if (i === 4) {
-        $("#showOutput4").html("");
-        $("#showOutput4").append("<h3>ManHunter contracts are here</h3>");
-        for (let l = 0; l < jobBoard.ourThreeDimentionalArray[i][0].length; l = l + 1) {
-          $("#showOutput4").append(`<li>Enemies present are ${jobBoard.ourThreeDimentionalArray[i][0][l]} man hunters and the contract is worth ${jobBoard.ourThreeDimentionalArray[i][1][l]}g, with death money valued at ${jobBoard.manHunterContractDeathMoney} gold. <button class="scb manHunter" id="${i},${l}">Select Contract</button></li>`);
-        }
-      }
-      if (i === 5) {
-        $("#showOutput5").html("");
-        $("#showOutput5").append(`<h3>Soldier contracts are here </h3>`);
-        for (let l = 0; l < jobBoard.ourThreeDimentionalArray[i][0].length; l = l + 1) {
-          $("#showOutput5").append(`<li>Enemies present are ${jobBoard.ourThreeDimentionalArray[i][0][l]} soldiers and the contract is worth ${jobBoard.ourThreeDimentionalArray[i][1][l]}g, with death money valued at ${jobBoard.soldierContractDeathMoney} gold. <button class="scb soldier" id="${i},${l}">Select Contract</button></li>`);
-        }
-      }
-      if (i === 6) {
-        $("#showOutput6").html("");
-        $("#showOutput6").append(`<h3>Ogre contracts are here </h3>`);
-        for (let l = 0; l < jobBoard.ourThreeDimentionalArray[i][0].length; l = l + 1) {
-          $("#showOutput6").append(`<li>Enemies present are ${jobBoard.ourThreeDimentionalArray[i][0][l]} ogres and the contract is worth ${jobBoard.ourThreeDimentionalArray[i][1][l]}g, with death money valued at ${jobBoard.ogreContractDeathMoney} gold. <button class="scb ogre" id="${i},${l}">Select Contract</button></li>`);
-        }
-      }
-      if (i === 7) {
-        $("#showOutput7").html("");
-        $("#showOutput7").append(`<h3>WarParty contracts are here </h3>`);
-        for (let l = 0; l < jobBoard.ourThreeDimentionalArray[i][0].length; l = l + 1) {
-          $("#showOutput7").append(`<li>Enemies present are ${jobBoard.ourThreeDimentionalArray[i][0][l]} knights and the contract is worth ${jobBoard.ourThreeDimentionalArray[i][1][l]}g, with death money valued at ${jobBoard.warPartyContractDeathMoney} gold. <button class="scb warParty" id="${i},${l}">Select Contract</button></li>`);
-        }
-      }
+      // if (i === 0) {
+      //   $("#showOutput0").html("");
+      //   $("#showOutput0").append("<h3>WereWolf contracts are here</h3>");
+      //   for (let l = 0; l < jobBoard.ourThreeDimentionalArray[i][0].length; l = l + 1) {
+      //     $("#showOutput0").append(`<li>Enemies present are ${jobBoard.ourThreeDimentionalArray[i][0][l]} werewolves and the contract is worth ${jobBoard.ourThreeDimentionalArray[i][1][l]}g, with death money valued at ${jobBoard.werewolfDeathMoney} gold. <button class="scb werewolf" id="${i},${l}">Select Contract</button></li>`);
+      //   }
+      // }
+      // if (i === 1) {
+      //   $("#showOutput1").html("");
+      //   $("#showOutput1").append("<h3>Goblin contracts are here</h3>");
+      //   for (let l = 0; l < jobBoard.ourThreeDimentionalArray[i][0].length; l = l + 1) {
+      //     $("#showOutput1").append(`<li> Enemies present are ${jobBoard.ourThreeDimentionalArray[i][0][l]} goblins and the contract is worth ${jobBoard.ourThreeDimentionalArray[i][1][l]}g, with death money valued at ${jobBoard.goblinCaveDeathMoney} gold. <button class="scb goblin" id="${i},${l}">Select Contract</button></li>`);
+      //   }
+      // }
+      // if (i === 2) {
+      //   $("#showOutput2").html("");
+      //   $("#showOutput2").append("<h3>Caravan Escort contracts are here</h3>");
+      //   for (let l = 0; l < jobBoard.ourThreeDimentionalArray[i][0].length; l = l + 1) {
+      //     $("#showOutput2").append(`<li> Enemies present are ${jobBoard.ourThreeDimentionalArray[i][0][l]} bandits and the contract is worth ${jobBoard.ourThreeDimentionalArray[i][1][l]}g, with death money valued at ${jobBoard.caravanEscortDeathMoney} gold. <button class="scb caravanEscort" id="${i},${l}">Select Contract</button></li>`);
+      //   }
+      // }
+      // if (i === 3) {
+      //   $("#showOutput3").html("");
+      //   $("#showOutput3").append("<h3>Caravan Ambush contracts are here</h3>");
+      //   for (let l = 0; l < jobBoard.ourThreeDimentionalArray[i][0].length; l = l + 1) {
+      //     $("#showOutput3").append(`<li> Enemies present are ${jobBoard.ourThreeDimentionalArray[i][0][l]} caravan guards and the contract is worth ${jobBoard.ourThreeDimentionalArray[i][1][l]}g, with death money valued at ${jobBoard.caravanAmbushDeathMoney} gold. <button class="scb caravanAmbush" id="${i},${l}">Select Contract</button></li>`);
+      //   }
+      // }
+      // if (i === 4) {
+      //   $("#showOutput4").html("");
+      //   $("#showOutput4").append("<h3>ManHunter contracts are here</h3>");
+      //   for (let l = 0; l < jobBoard.ourThreeDimentionalArray[i][0].length; l = l + 1) {
+      //     $("#showOutput4").append(`<li>Enemies present are ${jobBoard.ourThreeDimentionalArray[i][0][l]} man hunters and the contract is worth ${jobBoard.ourThreeDimentionalArray[i][1][l]}g, with death money valued at ${jobBoard.manHunterContractDeathMoney} gold. <button class="scb manHunter" id="${i},${l}">Select Contract</button></li>`);
+      //   }
+      // }
+      // if (i === 5) {
+      //   $("#showOutput5").html("");
+      //   $("#showOutput5").append(`<h3>Soldier contracts are here </h3>`);
+      //   for (let l = 0; l < jobBoard.ourThreeDimentionalArray[i][0].length; l = l + 1) {
+      //     $("#showOutput5").append(`<li>Enemies present are ${jobBoard.ourThreeDimentionalArray[i][0][l]} soldiers and the contract is worth ${jobBoard.ourThreeDimentionalArray[i][1][l]}g, with death money valued at ${jobBoard.soldierContractDeathMoney} gold. <button class="scb soldier" id="${i},${l}">Select Contract</button></li>`);
+      //   }
+      // }
+      // if (i === 6) {
+      //   $("#showOutput6").html("");
+      //   $("#showOutput6").append(`<h3>Ogre contracts are here </h3>`);
+      //   for (let l = 0; l < jobBoard.ourThreeDimentionalArray[i][0].length; l = l + 1) {
+      //     $("#showOutput6").append(`<li>Enemies present are ${jobBoard.ourThreeDimentionalArray[i][0][l]} ogres and the contract is worth ${jobBoard.ourThreeDimentionalArray[i][1][l]}g, with death money valued at ${jobBoard.ogreContractDeathMoney} gold. <button class="scb ogre" id="${i},${l}">Select Contract</button></li>`);
+      //   }
+      // }
+      // if (i === 7) {
+      //   $("#showOutput7").html("");
+      //   $("#showOutput7").append(`<h3>WarParty contracts are here </h3>`);
+      //   for (let l = 0; l < jobBoard.ourThreeDimentionalArray[i][0].length; l = l + 1) {
+      //     $("#showOutput7").append(`<li>Enemies present are ${jobBoard.ourThreeDimentionalArray[i][0][l]} knights and the contract is worth ${jobBoard.ourThreeDimentionalArray[i][1][l]}g, with death money valued at ${jobBoard.warPartyContractDeathMoney} gold. <button class="scb warParty" id="${i},${l}">Select Contract</button></li>`);
+      //   }
+      // }
     }
   });
 
@@ -123,7 +132,7 @@ $(document).ready(function () {
   $(".container").on("click", ".scb", function () {
     let i = this.id.split(",");
     let a = i[0];
-    let b = i[1];
+    let b = i[1]
 
     jobBoard.contractClicked[0] = jobBoard.ourThreeDimentionalArray[a][0][b];
     jobBoard.contractClicked[1] = jobBoard.ourThreeDimentionalArray[a][1][b];
@@ -197,7 +206,7 @@ $(document).ready(function () {
     $("#combatlog").text("");
     combat.combatLog = [];
     
-
+  console.log (`I am even here or not`);
     incomeStatement.numberOfMercenaries = ourCompany.length;
     let survived = combat.combat(ourCompany, filledArray);
     ourCompany = survived;
